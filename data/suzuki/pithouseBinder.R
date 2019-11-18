fc8aedd67282835d43ca660dee332118fa21e8a2
Enrico R. Crema <enrico.crema@gmail.com>
2019-11-18 19:34:53 +0000

    Added pithouse data manipulation scripts, output, and description

---
+++ data/suzuki/pithouseBinder.R
@@ -0,0 +1,32 @@
+# Read data from each prefecture
+nagano = read.csv("./nagano.csv",stringsAsFactors = FALSE)
+kanagawa = read.csv("./kanagawa.csv",stringsAsFactors = FALSE)
+yamanashi = read.csv("./yamanashi.csv",stringsAsFactors = FALSE)
+tokyo = read.csv("./tokyo.csv",stringsAsFactors = FALSE)
+saitama = read.csv("./saitama.csv",stringsAsFactors = FALSE)
+
+pthlist=list(Nagano=nagano,Kanagawa=kanagawa,Tokyo=tokyo,Saitama=saitama,Yamanashi=yamanashi)
+res=vector("list",length=length(pthlist))
+
+for (i in 1:length(pthlist))
+{
+  tmp = pthlist[[i]]
+  cases = unique(data.frame(st=tmp$Timespan_Start,en=tmp$Timespan_End))
+  ncases = nrow(cases)
+  cases$counts = NA
+  for (j in 1:ncases)
+  {
+    tmp2=subset(tmp,Timespan_Start==cases$st[j]&Timespan_End==cases$en[j])
+    cases$counts[j]=sum(tmp2$Counts)
+  }
+  cases$pref=names(pthlist)[[i]]
+  res[[i]]=cases
+  names(res)[[i]]=names(pthlist)[[i]]
+}
+
+res=lapply(res,orgTable) #orgTable converts aggregated counts into a data.frame with 1 house per row
+pithouseData=rbind.data.frame(res[[1]],res[[2]],res[[3]],res[[4]],res[[5]]) #combine to a single data.frame
+pithouseData$Region = "SWKanto"
+pithouseData$Region[which(pithouseData$pref%in%c("Yamanashi","Nagano"))]="ChubuHighlands"
+# save data into an R image
+save(pithouseData,file="../../R_images/pithouseData.RData")
--- log.R
+++ log.R
@@ -112,43 +112,8 @@ postTrapezoid=convertToArray(trapezoid.samples,type="trapezium",phases)
 save(postTrapezoid,postUniform,postGaussian,file="./R_images/posteriorSamples.RData")
 
 
-## Prepare Pithouse Data ####
-
-## Read Suzuki's dataset
-nagano = read.csv("./data/suzuki/nagano.csv",stringsAsFactors = FALSE)
-kanagawa = read.csv("./data/suzuki/kanagawa.csv",stringsAsFactors = FALSE)
-yamanashi = read.csv("./data/suzuki/yamanashi.csv",stringsAsFactors = FALSE)
-tokyo = read.csv("./data/suzuki/tokyo.csv",stringsAsFactors = FALSE)
-saitama = read.csv("./data/suzuki/saitama.csv",stringsAsFactors = FALSE)
-
-pthlist=list(Nagano=nagano,Kanagawa=kanagawa,Tokyo=tokyo,Saitama=saitama,Yamanashi=yamanashi)
-res=vector("list",length=length(pthlist))
-
-for (i in 1:length(pthlist))
-{
-  tmp = pthlist[[i]]
-  cases = unique(data.frame(st=tmp$Timespan_Start,en=tmp$Timespan_End))
-  ncases = nrow(cases)
-  cases$counts = NA
-  for (j in 1:ncases)
-  {
-    tmp2=subset(tmp,Timespan_Start==cases$st[j]&Timespan_End==cases$en[j])
-    cases$counts[j]=sum(tmp2$Counts)
-  }
-  cases$pref=names(pthlist)[[i]]
-  res[[i]]=cases
-  names(res)[[i]]=names(pthlist)[[i]]
-}
-
-res=lapply(res,orgTable) #orgTable converts aggregated counts into a data.frame with 1 house per row
-pithouseData=rbind.data.frame(res[[1]],res[[2]],res[[3]],res[[4]],res[[5]]) #combine to a single data.frame
-
-# save data into an R image
-save(pithouseData,file="./R_images/pithouseData.RData")
-
-
-
 ## Simulate Pithouse Dates ####
+load("./R_images/pithouseData.RData")
 simGaussian=mcsim(pithouseData[,-3],nsim=nsim,posterior=postGaussian,weights="variance")
 simUniform=mcsim(pithouseData[,-3],nsim=nsim,posterior=postUniform,weights="variance")
 simTrapezoid=mcsim(pithouseData[,-3],nsim=nsim,posterior=postTrapezoid,weights="variance")
